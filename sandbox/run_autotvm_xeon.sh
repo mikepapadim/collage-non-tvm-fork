a3d86b5375221c25b93a8ec99043a81746d6f124
Sunghyun Park <sunggg@umich.edu>
2021-08-24 09:29:57 +0900

    [AutoTVM Log] Tuning log for Xeon is ready except for ResNext and BERT

--- sandbox/run_autotvm_xeon.sh
+++ sandbox/run_autotvm_xeon.sh
@@ -12,9 +12,11 @@
 # CUDA_VISIBLE_DEVICES=0 --> RTX3070
 # --> RTX2070
 # python3 testing/autotune_relay.py -tu autotvm -t llvm -th llvm -dt float32 -n resnet50 -l autotvm_ops -bs 1 -hw xeon
-# python3 testing/autotune_relay.py -tu autotvm -t llvm -th llvm -dt float32 -n bert -l autotvm_ops -bs 1 -hw xeon
+# Error
+python3 testing/autotune_relay.py -tu autotvm -t llvm -th llvm -dt float32 -n bert -l autotvm_ops -bs 1 -hw xeon
+# Error
 python3 testing/autotune_relay.py -tu autotvm -t llvm -th llvm -dt float32 -n resnext50_32x4d -l autotvm_ops -bs 1 -hw xeon
-python3 testing/autotune_relay.py -tu autotvm -t llvm -th llvm -dt float32 -n nasneta -l autotvm_ops -bs 1 -hw xeon
-python3 testing/autotune_relay.py -tu autotvm -t llvm -th llvm -dt float32 -n nasrnn -l autotvm_ops -bs 1 -hw xeon
-python3 testing/autotune_relay.py -tu autotvm -t llvm -th llvm -dt float32 -n resnet50_3d -l autotvm_ops -bs 1 -hw xeon
-python3 testing/autotune_relay.py -tu autotvm -t llvm -th llvm -dt float32 -n mobilenet_v2 -l autotvm_ops -bs 1 -hw xeon
+#python3 testing/autotune_relay.py -tu autotvm -t llvm -th llvm -dt float32 -n nasneta -l autotvm_ops -bs 1 -hw xeon
+#python3 testing/autotune_relay.py -tu autotvm -t llvm -th llvm -dt float32 -n nasrnn -l autotvm_ops -bs 1 -hw xeon
+#python3 testing/autotune_relay.py -tu autotvm -t llvm -th llvm -dt float32 -n resnet50_3d -l autotvm_ops -bs 1 -hw xeon
+#python3 testing/autotune_relay.py -tu autotvm -t llvm -th llvm -dt float32 -n mobilenet_v2 -l autotvm_ops -bs 1 -hw xeon
