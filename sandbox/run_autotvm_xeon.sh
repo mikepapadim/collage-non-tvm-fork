4aee9e66c00abca5963d620e16837635ab7dfc91
Ubuntu <ubuntu@ip-172-31-41-124.us-west-2.compute.internal>
2021-09-16 05:09:07 +0000

    [Autotuning Xeon] Script updated accordingly, added DCGAN

--- sandbox/run_autotvm_xeon.sh
+++ sandbox/run_autotvm_xeon.sh
@@ -16,7 +16,8 @@
 python3 testing/autotune_relay.py -tu autotvm -t llvm -th llvm -dt float32 -n bert -l autotvm_ops -bs 1 -hw xeon
 # Error
 python3 testing/autotune_relay.py -tu autotvm -t llvm -th llvm -dt float32 -n resnext50_32x4d -l autotvm_ops -bs 1 -hw xeon
-#python3 testing/autotune_relay.py -tu autotvm -t llvm -th llvm -dt float32 -n nasneta -l autotvm_ops -bs 1 -hw xeon
-#python3 testing/autotune_relay.py -tu autotvm -t llvm -th llvm -dt float32 -n nasrnn -l autotvm_ops -bs 1 -hw xeon
-#python3 testing/autotune_relay.py -tu autotvm -t llvm -th llvm -dt float32 -n resnet50_3d -l autotvm_ops -bs 1 -hw xeon
+python3 testing/autotune_relay.py -tu autotvm -t llvm -th llvm -dt float32 -n nasneta -l autotvm_ops -bs 1 -hw xeon
+python3 testing/autotune_relay.py -tu autotvm -t llvm -th llvm -dt float32 -n resnet50_3d -l autotvm_ops -bs 1 -hw xeon
+python3 testing/autotune_relay.py -tu autotvm -t llvm -th llvm -dt float32 -n dcgan -l autotvm_ops -bs 1 -hw xeon
 #python3 testing/autotune_relay.py -tu autotvm -t llvm -th llvm -dt float32 -n mobilenet_v2 -l autotvm_ops -bs 1 -hw xeon
+#python3 testing/autotune_relay.py -tu autotvm -t llvm -th llvm -dt float32 -n nasrnn -l autotvm_ops -bs 1 -hw xeon
